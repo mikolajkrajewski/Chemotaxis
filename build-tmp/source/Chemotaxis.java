ef063eb6b0dc0c52a3b66ad49605ccbf3b261773
unknown <mikrajewski@697-334-Edoras.sf.k12.internal>
2015-09-17 11:37:12 -0700

    chemotaxis

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 Bacteria [] jediKnights;
 int r,g,b;
- void setup()   
+ public void setup()   
  {     
  	size(300,300);  
  	jediKnights = new Bacteria[10];
@@ -9,7 +25,7 @@ int r,g,b;
  		jediKnights[i] = new Bacteria(150,150);
  	}
  }   
- void draw()   
+ public void draw()   
  {    
  	background(0);
  	for(int i = 0; i<jediKnights.length; i++)
@@ -29,16 +45,25 @@ int r,g,b;
  		//b = (int)(Math.random()*256);
  		//g = (int)(Math.random()*256);
  	}
- 	void show()
+ 	public void show()
  	{
  		strokeWeight(5);
  		stroke((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
  		noFill();
  		ellipse(myX,myY,20,20);
  	}
- 	void move()
+ 	public void move()
  	{
  		myX = myX + (int)(Math.random()*5)-2;
  		myY = myY + (int)(Math.random()*5)-2;
  	}
  }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
