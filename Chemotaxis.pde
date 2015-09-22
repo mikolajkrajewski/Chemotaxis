57316f0b88345edcc27110ea39334aa51c806264
unknown <mikrajewski@697-334-Edoras.sf.k12.internal>
2015-09-22 11:35:38 -0700

    chemotaxis

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -2,65 +2,79 @@ Bacteria [] jediKnights;
 Predator jango;
  void setup()   
  {     
- 	size(600,600);  
- 	jediKnights = new Bacteria[100];
- 	for(int i = 0; i<jediKnights.length; i++)
- 	{
- 		jediKnights[i] = new Bacteria((int)(Math.random()*500)+50,(int)(Math.random()*500)+50);
- 	}
- 	jango = new Predator(mouseX,mouseY);
+   size(600,600);  
+   jediKnights = new Bacteria[200];
+   for(int i = 0; i<jediKnights.length; i++)
+   {
+     jediKnights[i] = new Bacteria((int)(Math.random()*600),(int)(Math.random()*600));
+   }
+   jango = new Predator(mouseX,mouseY);
  }   
  void draw()   
  {    
- 	background(0);
- 	for(int i = 0; i<jediKnights.length; i++)
- 	{
- 		jediKnights[i].move();
- 		jediKnights[i].show();
- 	}
- 	jango.predMove();
- 	jango.predShow();
+   background(0);
+   for(int i = 0; i<jediKnights.length; i++)
+   {
+     jediKnights[i].move();
+     jediKnights[i].show();
+   }
+   jango.predMove();
+   jango.predShow();
  }  
  class Bacteria    
  {     
- 	int myX, myY, myColor;
- 	Bacteria(int x, int y)
- 	{
- 		myX = x;
- 		myY = y;
- 		myColor = color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
- 	}
- 	void show()
- 	{
- 		strokeWeight(5);
- 		stroke(myColor);
- 		noFill();
- 		ellipse(myX,myY,20,20);
- 	}
- 	void move()
- 	{
- 		myX = myX + (int)(Math.random()*7)-3;
- 		myY = myY + (int)(Math.random()*7)-3;
- 	}
+   int myX, myY, myColor;
+   Bacteria(int x, int y)
+   {
+     myX = x;
+     myY = y;
+     myColor = color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
+   }
+   void show()
+   {
+     strokeWeight(5);
+     stroke(myColor);
+     noFill();
+     ellipse(myX,myY,20,20);
+   }
+   void move()
+   {
+     if(myX < mouseX)
+     {
+       myX += (int)(Math.random()*2);
+     }
+     else
+     {
+       myX += (int)(Math.random()*2)-1;
+     }
+     if(myY < mouseY)
+     {
+       myY += (int)(Math.random()*2);
+     }
+     else
+     {
+       myY += (int)(Math.random()*2)-1;
+     }
+   }
  }    
  class Predator
  {
- 	int mypX, mypY, mypColor;
- 	Predator(int pX, int pY)
- 	{
- 		mypX = pX;
- 		mypY = pY;
- 		mypColor = color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
- 	}
- 	void predShow()
- 	{
- 		noStroke();
- 		fill(mypColor);
- 		ellipse(mypX,mypY,50,50);
- 	}
- 	void predMove()
- 	{
- 		mypX = mouseX + (int)(Math.random()*7)-3;
- 		mypY = mouseY + (int)(Math.random()*7)-3;
- 	}
+   int mypX, mypY, mypColor;
+   Predator(int pX, int pY)
+   {
+     mypX = pX;
+     mypY = pY;
+     mypColor = color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
+   }
+   void predShow()
+   {
+     noStroke();
+     fill(mypColor);
+     ellipse(mypX,mypY,50,50);
+   }
+   void predMove()
+   {
+     mypX = mouseX + (int)(Math.random()*7)-3;
+     mypY = mouseY + (int)(Math.random()*7)-3;
+   }
  }
--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -18,67 +18,81 @@ Bacteria [] jediKnights;
 Predator jango;
  public void setup()   
  {     
- 	size(600,600);  
- 	jediKnights = new Bacteria[100];
- 	for(int i = 0; i<jediKnights.length; i++)
- 	{
- 		jediKnights[i] = new Bacteria((int)(Math.random()*500)+50,(int)(Math.random()*500)+50);
- 	}
- 	jango = new Predator(mouseX,mouseY);
+   size(600,600);  
+   jediKnights = new Bacteria[200];
+   for(int i = 0; i<jediKnights.length; i++)
+   {
+     jediKnights[i] = new Bacteria((int)(Math.random()*600),(int)(Math.random()*600));
+   }
+   jango = new Predator(mouseX,mouseY);
  }   
  public void draw()   
  {    
- 	background(0);
- 	for(int i = 0; i<jediKnights.length; i++)
- 	{
- 		jediKnights[i].move();
- 		jediKnights[i].show();
- 	}
- 	jango.predMove();
- 	jango.predShow();
+   background(0);
+   for(int i = 0; i<jediKnights.length; i++)
+   {
+     jediKnights[i].move();
+     jediKnights[i].show();
+   }
+   jango.predMove();
+   jango.predShow();
  }  
  class Bacteria    
  {     
- 	int myX, myY, myColor;
- 	Bacteria(int x, int y)
- 	{
- 		myX = x;
- 		myY = y;
- 		myColor = color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
- 	}
- 	public void show()
- 	{
- 		strokeWeight(5);
- 		stroke(myColor);
- 		noFill();
- 		ellipse(myX,myY,20,20);
- 	}
- 	public void move()
- 	{
- 		myX = myX + (int)(Math.random()*7)-3;
- 		myY = myY + (int)(Math.random()*7)-3;
- 	}
+   int myX, myY, myColor;
+   Bacteria(int x, int y)
+   {
+     myX = x;
+     myY = y;
+     myColor = color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
+   }
+   public void show()
+   {
+     strokeWeight(5);
+     stroke(myColor);
+     noFill();
+     ellipse(myX,myY,20,20);
+   }
+   public void move()
+   {
+     if(myX < mouseX)
+     {
+       myX += (int)(Math.random()*2);
+     }
+     else
+     {
+       myX += (int)(Math.random()*2)-1;
+     }
+     if(myY < mouseY)
+     {
+       myY += (int)(Math.random()*2);
+     }
+     else
+     {
+       myY += (int)(Math.random()*2)-1;
+     }
+   }
  }    
  class Predator
  {
- 	int mypX, mypY, mypColor;
- 	Predator(int pX, int pY)
- 	{
- 		mypX = pX;
- 		mypY = pY;
- 		mypColor = color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
- 	}
- 	public void predShow()
- 	{
- 		noStroke();
- 		fill(mypColor);
- 		ellipse(mypX,mypY,50,50);
- 	}
- 	public void predMove()
- 	{
- 		mypX = mouseX + (int)(Math.random()*7)-3;
- 		mypY = mouseY + (int)(Math.random()*7)-3;
- 	}
+   int mypX, mypY, mypColor;
+   Predator(int pX, int pY)
+   {
+     mypX = pX;
+     mypY = pY;
+     mypColor = color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
+   }
+   public void predShow()
+   {
+     noStroke();
+     fill(mypColor);
+     ellipse(mypX,mypY,50,50);
+   }
+   public void predMove()
+   {
+     mypX = mouseX + (int)(Math.random()*7)-3;
+     mypY = mouseY + (int)(Math.random()*7)-3;
+   }
  }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Chemotaxis" };
